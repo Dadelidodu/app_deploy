a2830df5e11c4b795425726e773cb84a69eff95e
Dadelidodu <anselotdavid@gmail.com>
2024-10-25 16:25:35 +0200

    rescramble update

--- app_run.py
+++ app_run.py
@@ -1,7 +1,3 @@
-
-# Extra App for user to interact with (Custom & GPT help)
-
-  
 import streamlit as st
 import io
 import pandas as pd
@@ -19,6 +15,9 @@ def main():
     # File upload widget
     uploaded_file = st.file_uploader("Upload your xlsx file with names", type="xlsx")
 
+    # Variable to track arrangement
+    arrangement_df = None
+
     if uploaded_file and set_tables and set_seats:
         # Load the uploaded Excel file into a DataFrame
         df = pd.read_excel(uploaded_file)
@@ -27,33 +26,31 @@ def main():
             
             # Create an Openspace instance and organize seating
             op = Openspace(set_tables, set_seats)
-            op.organize(file_path)
             
+            # Check if 'Rescramble' button is clicked to reshuffle
+            if st.button("Rescramble") or arrangement_df is None:
+                op.organize(file_path)
+            
+            # Get the seating arrangement DataFrame from the Openspace instance
+            arrangement_df = op.display()  # Get the DataFrame directly
+
             # Display organized seating arrangement
             st.subheader("Seating Arrangement")
-            arrangement_df = op.display()  # Get the DataFrame directly
-            
-            # Display the DataFrame in Streamlit
             if not arrangement_df.empty:
                 st.dataframe(arrangement_df)
 
             # Save organized arrangement to a file
-            #save_path = 'Random_distribution_output.xlsx'
-            #op.store(save_path)
-            #st.success(f"Arrangement saved to {save_path}")
-
-    # Create a BytesIO object to save the DataFrame to a CSV file
             output = io.StringIO()
             arrangement_df.to_csv(output, index=False)
             output.seek(0)
 
-                # Add a download button for the CSV
+            # Add a download button for the CSV
             st.download_button(
-                    label="Download Arrangement as CSV",
-                    data=output.getvalue(),
-                    file_name='Random_distribution_output.csv',
-                    mime='text/csv'
-                )    
+                label="Download Arrangement as CSV",
+                data=output.getvalue(),
+                file_name='Random_distribution_output.csv',
+                mime='text/csv'
+            )    
         else:
             st.error("The uploaded file must contain a 'Names' column.")
     else:
@@ -61,5 +58,4 @@ def main():
 
 main()
 
-    # Run this command in Terminal for running the app :
-    # streamlit run C:\Users\HP\Desktop\FraDaMi\app_run.py
+# streamlit run C:\Users\HP\Desktop\app_deploy\app_run.py
